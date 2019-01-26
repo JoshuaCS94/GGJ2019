ddb81f4634bcbb96d132998e399b51077f7be98e
Josue Cortina Suarez <jcortina781@gmail.com>
2019-01-26 17:09:36 -0500

    Add Home scene

---
+++ Assets/Scenes/1. Home/Scripts/HomeManager.cs
@@ -0,0 +1,19 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeManager : MonoBehaviour
+{
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+
+	public void StartGame()
+	{
+		var a = FindObjectOfType<SceneTransitioner>();
+		a.FadeBlack().OnComplete(() =>
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1));
+
+	}
+}
