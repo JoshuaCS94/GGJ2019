6f7cc847dcf2292cdd7101dfcbbf6dddee6fadf0
Carlos David <udl.was.here@gmail.com>
2019-01-26 12:47:37 -0500

    Add House Scene

---
+++ Assets/Scenes/House/DoorManger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManger : MonoBehaviour
+{
+    public bool open = false;
+    public int noknok_count = 10;
+    public int max_noknok = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NokNok()
+    {
+        noknok_count--;
+        if (noknok_count == 0)
+        {
+            open = true;
+            noknok_count = max_noknok;
+        }
+    }
+
+
+    public void Open()
+    {
+        
+    }
+
+    public void Close()
+    {
+        
+    }
+}
