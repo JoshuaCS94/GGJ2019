bfb1a3d1bf192b751540a6d7a5683a27f853b0ab
Carlos David <udl.was.here@gmail.com>
2019-01-26 15:11:13 -0500

    Finish door manager

--- Assets/Scenes/House/DoorManger.cs
+++ Assets/Scenes/House/DoorManger.cs
@@ -1,25 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class DoorManger : MonoBehaviour
 {
     public bool open = false;
     public int noknok_count = 10;
     public int max_noknok = 10;
+    public bool reverse = false;
 
-    public Sprite Close;
-    public Sprite Broken;
+    public Sprite close;
+    public Sprite broken;
     
-    private Sprite image;
+    private SpriteRenderer image;
 
     private BoxCollider2D col;
     
     // Start is called before the first frame update
     void Start()
     {
-        image = GetComponent<Sprite>();
         col = GetComponent<BoxCollider2D>();
+        if (reverse)
+        {
+            transform.DORotate(new Vector3(0, 180, 0), 0.2f);
+        }
     }
 
     // Update is called once per frame
@@ -39,13 +44,32 @@ public class DoorManger : MonoBehaviour
     }
 
 
-    public void Open()
+    public void Fix()
     {
-        
+        col.enabled = true;
+        if (reverse)
+        {
+            transform.DOMoveX(-13.5f, 0.2f);
+        }
+        else
+        {
+            transform.DOMoveX(-13.5f, 0.2f);
+        }
+        GetComponent<SpriteRenderer>().sprite = close;
     }
 
     public void Break()
     {
-        
+        col.enabled = false;
+        if (reverse)
+        {
+            transform.DOMoveX(12.5f, 0.2f);
+        }
+        else
+        {
+            transform.DOMoveX(-12.5f, 0.2f);
+        }
+        GetComponent<SpriteRenderer>().sprite = broken;
     }
+    
 }
