c8280597ac866e056311b6d606685bc2e2c24ce0
Carlos David <udl.was.here@gmail.com>
2019-01-26 14:19:00 -0500

    Add doors management

--- Assets/Scenes/House/DoorManger.cs
+++ Assets/Scenes/House/DoorManger.cs
@@ -7,10 +7,19 @@ public class DoorManger : MonoBehaviour
     public bool open = false;
     public int noknok_count = 10;
     public int max_noknok = 10;
+
+    public Sprite Close;
+    public Sprite Broken;
+    
+    private Sprite image;
+
+    private BoxCollider2D col;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        image = GetComponent<Sprite>();
+        col = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -35,7 +44,7 @@ public class DoorManger : MonoBehaviour
         
     }
 
-    public void Close()
+    public void Break()
     {
         
     }
