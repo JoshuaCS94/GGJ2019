a8db52bd8832e4c4790eba7ef029ed57b5db62c4
Carlos David <udl.was.here@gmail.com>
2019-01-27 12:03:18 -0500

    Add repell mechanism

---
+++ Assets/Repell_enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repell_enemy : SpawnPoint
+{
+    public bool is_Touching => col.IsTouchingLayers(LayerMask.NameToLayer("Player"));
+
+    private Collider2D col;
+    
+    private void Awake()
+    {
+        col = GetComponent<Collider2D>();
+    }
+
+    public override void Spawn()
+    {
+        if(!is_Touching)
+            base.Spawn();
+    }
+
+}
