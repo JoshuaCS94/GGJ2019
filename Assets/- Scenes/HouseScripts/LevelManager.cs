d1336d2e34fd3ad2ed3b641fa3f8641fcf8d99ea
Carlos David <udl.was.here@gmail.com>
2019-01-27 15:04:11 -0500

    idk commit

--- Assets/- Scenes/HouseScripts/LevelManager.cs
+++ Assets/- Scenes/HouseScripts/LevelManager.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Mime;
 using UnityEngine;
+using System.Timers;
+using UnityEngine.UI;
+using DG.Tweening;
+
 
 public class LevelManager : MonoBehaviour
 {
     public float enemy_spaw_rating = 10;
     public List<SpawnPoint> spawns;
-
+    public Stopwatch timer;
+    
     public int enemy_count = 0;
 
     public float spawn_time = 10;
+
+    public int max_enemies = 15;
+
+    public Text time_text;
+    public Text enemy_count_text;
     
     System.Random r = new System.Random();
     
@@ -17,12 +29,24 @@ public class LevelManager : MonoBehaviour
     void Start()
     {
         StartCoroutine("SpawnEnemys");
+        timer = new Stopwatch();
+        timer.Start();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        float time = timer.ElapsedMilliseconds / 1000f;
+        time_text.DOText(time.ToString(), 0.1f);
         
+        enemy_count_text.DOText(enemy_count.ToString(), 0.2f);
+        //enemy_count_text.DOColor()
+
+        if (enemy_count > max_enemies)
+        {
+            timer.Stop();
+        }
     }
     
     public void EnemyDead()
